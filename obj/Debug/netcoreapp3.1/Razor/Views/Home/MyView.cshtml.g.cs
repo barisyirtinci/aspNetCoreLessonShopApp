31619ffb2ff95582c71a8acc528e07f5dba9870b
barisyirtinci <brsyrtnc@gmail.com>
2023-02-05 15:46:49 +0300

    partial view update

--- obj/Debug/netcoreapp3.1/Razor/Views/Home/MyView.cshtml.g.cs
+++ obj/Debug/netcoreapp3.1/Razor/Views/Home/MyView.cshtml.g.cs
@@ -13,14 +13,14 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
 #nullable restore
-#line 1 "C:\Users\brsyr\shopapp\shopapp.webui\Views\_ViewImports.cshtml"
+#line 2 "C:\Users\brsyr\shopapp\shopapp.webui\Views\_ViewImports.cshtml"
 using shopapp.webui.Models;
 
 #line default
 #line hidden
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"5d7f12d9a0cfa96a856d0fcdbe1d8f0c2314699d", @"/Views/Home/MyView.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"a6a2f9735f8df7cb46870cb1a0a73ac5a4e75cdb", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"415ce9953bd46be8f2c4fe4cc22e26d003bfb282", @"/Views/_ViewImports.cshtml")]
     public class Views_Home_MyView : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #line hidden
